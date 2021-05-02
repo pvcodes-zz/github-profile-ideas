ddc9d85c6679d4da742bdceb3fec1d8595d5f6ef
yashsehgal <yashsehgal.work@gmail.com>
2021-05-03 02:42:57 +0530

    card views added

--- Views/app.js
+++ Views/app.js
@@ -6,9 +6,41 @@ fetch('https://raw.githubusercontent.com/DesignSystemsOSS/github-profile-ideas/m
   .then((res) => res.json())
   .then((res) => {
   for (let profile_data in res) {
-    console.log(res[profile_data])
+    console.log(res[profile_data]);
+
+    let profileCard = document.createElement("div");
+    profileCard.classList.add("profile-card");
+
+    let profile_username = document.createElement("h2");
+    profile_username.classList.add('profile-username');
+
+    let profile_fullname = document.createElement("p");
+    profile_fullname.classList.add('profile-fullname');
+
+    let profile_photo = document.createElement("img");
+    profile_photo.classList.add("profile-photo");
+
+    let profile_screenshot = document.createElement("img");
+    profile_screenshot.classList.add("profile-screenshot");
+    
+    profile_username.textContent = `@${res[profile_data].github_username}`;
+    profile_fullname.textContent = res[profile_data].fullname;
+    profile_photo.src = res[profile_data].profile_photo;
+    profile_screenshot.src = res[profile_data].profile_screenshot;
+
+    profileCard.append(profile_screenshot);
+
+    let profileCardBody = document.createElement("div");
+    profileCardBody.classList.add("profile-card-body");
+
+    profileCardBody.append(profile_photo);
+    profileCardBody.append(profile_username);
+    
+    profileCard.append(profileCardBody);
+    
+    profileCard.append(profile_fullname);
+    
+    ProfileContainer.append(profileCard);
   }
 });
-let profileCard = document.createElement("div");
-profileCard.classList.add("profile-card");
 
